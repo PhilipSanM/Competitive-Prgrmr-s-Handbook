ba1389eb4745328b8534314557e8bb2bc7eb1bd8
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-01-27 20:22:39 -0600

    c; I doesn't commit

--- Basicos/Monk.cpp
+++ Basicos/Monk.cpp
@@ -7,18 +7,41 @@ using namespace std;
 
 // #include<queue>
  
+// int main(){
+// 	// priority_queue<int> pq;
+// 	priority_queue<int> pq;
+
+
+// 	cout << snume << endl;
+// 	pq.push(2);
+// 	pq.push(5);
+// 	pq.push(-4);
+// 	pq.push(1);
+
+// 	cout << "\n\n";
+// 	while(!pq.empty()){
+// 		cout << "[" << pq.top() << "]\n";
+// 		pq.pop();
+// 	}
+// 	return 0;
+// }
+
+
+ 
 int main(){
+	// priority_queue<int> pq;
 	priority_queue<int> pq;
-	int snume;
-	cin >> snume;
+
+
 	cout << snume << endl;
 	pq.push(2);
 	pq.push(5);
-
+	pq.push(-4);
+	pq.push(1);
 
 	cout << "\n\n";
 	while(!pq.empty()){
-		cout << "----" << pq.top() << "\n";
+		cout << "[" << pq.top() << "]\n";
 		pq.pop();
 	}
 	return 0;
