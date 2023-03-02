242485750c95198ffe4cfc8bb397e2e21160abf3
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-03-02 08:15:02 -0600

    217

---
+++ NeetCode150/217_ContainsDuplicate.py
@@ -0,0 +1,16 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# =-=-=-=-=-=-=- 217. Contains Duplicate -=-=-=-=-=-=-=-=-=-=-==
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Easy
+# 
+# Date: March - 03 - 2023
+# URL: https://leetcode.com/problems/contains-duplicate/
+# ====================================================================
+
+
+# First Approach - Using a hash-map
+# Time Complexity : O(n)
+# Space Complexity: O(n)
+
+
+# POST: https://leetcode.com/problems/contains-duplicate/solutions/3247939/python3-easy-hashmap/
