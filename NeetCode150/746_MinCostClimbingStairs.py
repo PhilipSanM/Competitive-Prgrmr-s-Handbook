7a76982bac2b206ddfaa58b5a7dfdcc8f7876947
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-06-29 12:38:02 -0600

    746

---
+++ NeetCode150/746_MinCostClimbingStairs.py
@@ -0,0 +1,34 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# =-=-=-=-=-=-=- 746. Min Cost Climbing Stairs -=-=-=-=-=-=-=-=-=-=-==
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Easy
+# 
+# Date: June - 29 - 2023
+# URL: https://leetcode.com/problems/min-cost-climbing-stairs/description/
+# ====================================================================
+
+# First Approach - 1-DP
+# Time Complexity : O(n)
+# Space Complexity: O(1)
+
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        # [1   ,100   ,1   ,1   ,1    ,100   ,1   ,1   ,100    ,1]
+        # [0, 0 ]
+        if len(cost) < 2:
+            return cost[0]
+        if len(cost) < 3:
+            return min(cost[0],cost[1])
+
+        dp = [cost[0], cost[1]]
+        for i in range(2, len(cost) - 1):
+            temp = min(dp[0] + cost[i], dp[1] + cost[i])
+            dp[0] = dp[1]
+            dp[1] = temp
+
+        return min(dp[1], dp[0] + cost[-1])
