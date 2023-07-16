b79cd105642451483eb4bd51591cd88d3ee0d1b6
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2023-07-16 17:56:27 -0600

    3. Longest Substring Without Repeating Characters

--- Leetcode/TopInterviewQuestions/3_LongestSubstringWithoutRepeatingCharacters.py
+++ Leetcode/TopInterviewQuestions/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -8,7 +8,7 @@
 # ====================================================================
 
 
-#  My Solution
+#  nettcode sol
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
@@ -38,3 +38,35 @@ class Solution(object):
 
 # Time complexity: O(N)
 # Space Complexity: O(N)
+
+
+# My solution
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_size = 0
+        curr_size = 0
+        dic_string = {}
+        first = 0
+        second = 0
+        for i in range(0, len(s)):
+            if s[second] in dic_string:
+                # Loop first point one position next to the repeating char
+                # Remove one element in currsize update first poniter until passing repeating o
+                while s[second] in dic_string: 
+                    dic_string.pop(s[first])
+                    curr_size -= 1
+                    first += 1
+            dic_string[s[second]] = i
+            second += 1
+            curr_size += 1
+
+            max_size = max(max_size, curr_size)
+            
+
+
+        return max_size
